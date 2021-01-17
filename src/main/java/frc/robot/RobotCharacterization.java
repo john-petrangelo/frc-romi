a14fa2fc83e6ee3406938438647ee8d745d3da93
John Petrangelo <john_petrangelo@bose.com>
2021-01-17 11:39:24 -0500

    Added RobotCharacterization class from frc-characterization tool, with some updates

---
+++ src/main/java/frc/robot/RobotCharacterization.java
@@ -0,0 +1,226 @@
+/**
+* This is a very simple robot program that can be used to send telemetry to
+* the data_logger script to characterize your drivetrain. If you wish to use
+* your actual robot code, you only need to implement the simple logic in the
+* autonomousPeriodic function and change the NetworkTables update rate
+*/
+
+package frc.robot;
+
+import java.util.function.Supplier;
+
+// WPI_Talon* imports are needed in case a user has a Pigeon on a Talon
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.sensors.RomiGyro;
+
+import java.util.ArrayList; 
+
+public class RobotCharacterization extends TimedRobot {
+
+  static private double ENCODER_EDGES_PER_REV = 1440 / 4.;
+  static private double GEARING = 1;
+  
+  private double encoderConstant = (1 / GEARING) * (1 / ENCODER_EDGES_PER_REV);
+
+  Joystick stick;
+  DifferentialDrive drive;
+
+
+  Supplier<Double> leftEncoderPosition;
+  Supplier<Double> leftEncoderRate;
+  Supplier<Double> rightEncoderPosition;
+  Supplier<Double> rightEncoderRate;
+  RomiGyro gyro;
+
+  NetworkTableEntry autoSpeedEntry = NetworkTableInstance.getDefault().getEntry("/robot/autospeed");
+  NetworkTableEntry telemetryEntry = NetworkTableInstance.getDefault().getEntry("/robot/telemetry");
+  NetworkTableEntry rotateEntry = NetworkTableInstance.getDefault().getEntry("/robot/rotate");
+
+  int counter = 0;
+  double startTime = 0;
+  double priorAutospeed = 0;
+
+  double[] numberArray = new double[10];
+  ArrayList<Double> entries = new ArrayList<Double>();
+  public RobotCharacterization() {
+    super(.005);
+    LiveWindow.disableAllTelemetry();
+  }
+
+  public enum Sides {
+    LEFT,
+    RIGHT,
+    FOLLOWER
+  }
+
+  // methods to create and setup motors (reduce redundancy)
+  public Spark setupSpark(int port, Sides side, boolean inverted) {
+    // create new motor and set neutral modes (if needed)
+    Spark motor = new Spark(port);
+    motor.setInverted(inverted);
+    
+    // setup encoder if motor isn't a follower
+    if (side != Sides.FOLLOWER) {
+
+      Encoder encoder;
+      switch (side) {
+        // setup encoder and data collecting methods
+
+        case RIGHT:
+          // set right side methods = encoder methods
+
+          encoder = new Encoder(6, 7);
+          encoder.setReverseDirection(false);
+
+          encoder.setDistancePerPulse(encoderConstant);
+          rightEncoderPosition = encoder::getDistance;
+          rightEncoderRate = encoder::getRate;
+
+          break;
+        case LEFT:
+          encoder = new Encoder(4, 5);
+          encoder.setReverseDirection(false);
+          encoder.setDistancePerPulse(encoderConstant);
+          leftEncoderPosition = encoder::getDistance;
+          leftEncoderRate = encoder::getRate;
+
+          break;
+        default:
+          // probably do nothing
+          break;
+      }
+    }
+    
+    return motor;
+  }
+
+  @Override
+  public void robotInit() {
+    stick = new Joystick(0);
+    
+    // Create drivetrain.
+    Spark leftMotor = setupSpark(0, Sides.LEFT, false);
+    Spark rightMotor = setupSpark(1, Sides.RIGHT, false);
+    drive = new DifferentialDrive(leftMotor, rightMotor);
+    drive.setDeadband(0);
+
+    // Configure gyro
+    gyro = new RomiGyro();
+
+    // Set the update rate instead of using flush because of a ntcore bug
+    // -> probably don't want to do this on a robot in competition
+    NetworkTableInstance.getDefault().setUpdateRate(0.010);
+  }
+
+  @Override
+  public void disabledInit() {
+    double elapsedTime = Timer.getFPGATimestamp() - startTime;
+    System.out.println("Robot disabled");
+    drive.tankDrive(0, 0);
+    // data processing step
+    String data = entries.toString();
+    data = data.substring(1, data.length() - 1) + ", ";
+    telemetryEntry.setString(data);
+    entries.clear();
+    System.out.println("Robot disabled");
+    System.out.println("Collected : " + counter + " in " + elapsedTime + " seconds");
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+    // feedback for users, but not used by the control program
+    SmartDashboard.putNumber("l_encoder_pos", leftEncoderPosition.get());
+    SmartDashboard.putNumber("l_encoder_rate", leftEncoderRate.get());
+    SmartDashboard.putNumber("r_encoder_pos", rightEncoderPosition.get());
+    SmartDashboard.putNumber("r_encoder_rate", rightEncoderRate.get());
+    SmartDashboard.putNumber("gyro.getRoll", gyro.getAngleRoll());
+    SmartDashboard.putNumber("gyro.getPitch", gyro.getAnglePitch());
+    SmartDashboard.putNumber("gyro.getYaw", gyro.getAngleYaw());
+  }
+
+  @Override
+  public void teleopInit() {
+    System.out.println("Robot in operator control mode");
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    drive.arcadeDrive(-stick.getY(), stick.getX());
+  }
+
+  @Override
+  public void autonomousInit() {
+    System.out.println("Robot in autonomous mode");
+    startTime = Timer.getFPGATimestamp();
+    counter = 0;
+  }
+
+  /**
+  * If you wish to just use your own robot program to use with the data logging
+  * program, you only need to copy/paste the logic below into your code and
+  * ensure it gets called periodically in autonomous mode
+  * 
+  * Additionally, you need to set NetworkTables update rate to 10ms using the
+  * setUpdateRate call.
+  */
+  @Override
+  public void autonomousPeriodic() {
+
+    // Retrieve values to send back before telling the motors to do something
+    double now = Timer.getFPGATimestamp();
+
+    double leftPosition = leftEncoderPosition.get();
+    double leftRate = leftEncoderRate.get();
+
+    double rightPosition = rightEncoderPosition.get();
+    double rightRate = rightEncoderRate.get();
+
+    double battery = RobotController.getBatteryVoltage();
+    double motorVolts = battery * Math.abs(priorAutospeed);
+
+    double leftMotorVolts = motorVolts;
+    double rightMotorVolts = motorVolts;
+
+    // Retrieve the commanded speed from NetworkTables
+    double autospeed = autoSpeedEntry.getDouble(0);
+    priorAutospeed = autospeed;
+
+    // command motors to do things
+    drive.tankDrive(
+      (rotateEntry.getBoolean(false) ? -1 : 1) * autospeed, autospeed,
+      false
+    );
+
+    numberArray[0] = now;
+    numberArray[1] = battery;
+    numberArray[2] = autospeed;
+    numberArray[3] = leftMotorVolts;
+    numberArray[4] = rightMotorVolts;
+    numberArray[5] = leftPosition;
+    numberArray[6] = rightPosition;
+    numberArray[7] = leftRate;
+    numberArray[8] = rightRate;
+    numberArray[9] = gyro.getAngleYaw();
+
+    // Add data to a string that is uploaded to NT
+    for (double num : numberArray) {
+      entries.add(num);
+    }
+    counter++;
+  }
+}
