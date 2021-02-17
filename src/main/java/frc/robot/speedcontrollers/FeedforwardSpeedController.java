20a6a83c99a737c238795e242e59a7bcf47b9168
John Petrangelo <john_petrangelo@bose.com>
2021-02-16 23:00:44 -0500

    Don't log while speed=0.0

--- src/main/java/frc/robot/speedcontrollers/FeedforwardSpeedController.java
+++ src/main/java/frc/robot/speedcontrollers/FeedforwardSpeedController.java
@@ -35,12 +35,14 @@ public class FeedforwardSpeedController implements SpeedController {
             System.out.printf("FF[F%s] speed=%5.3f ffV=%5.3f\n", name, speed, ffV);
             SmartDashboard.putNumber(name + "-FF volts", ffV);
             SmartDashboard.putNumber(name + "-PID volts", 0);
-        } else {
+        } else if (speed < 0.0) {
             double ffV = backFF.calculate(speed);
             other.setVoltage(ffV);
             System.out.printf("FF[B%s] speed=%5.3f ffV=%5.3f\n", name, speed, ffV);
             SmartDashboard.putNumber(name + "-FF volts", ffV);
             SmartDashboard.putNumber(name + "-PID volts", 0);
+        } else {
+            other.setVoltage(0.0);
         }
     }
 
