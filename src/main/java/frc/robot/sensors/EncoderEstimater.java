cafb10b68cdacad9427231db650df28bb311208c
John Petrangelo <>
2021-04-02 21:47:29 -0400

    Initial EncoderEstimator attempt

--- src/main/java/frc/robot/sensors/EncoderEstimater.java
+++ src/main/java/frc/robot/sensors/EncoderEstimater.java
@@ -29,34 +29,60 @@ public class EncoderEstimater {
     }
 
     /**
-     * Gets the latest value from the encoder and updates internal state.
+     * Alternate constructor for unit tests. Instead of encoder, take initial distance and rate.
+     */
+    public EncoderEstimater(long initialTime, double initialDistance, double initialRate,
+            double modelDistance, double modelRate, double modelAccel) {
+        this.encoder = null;
+
+        lastKnownDistanceTime = initialTime;
+        lastKnownDistance = initialDistance;
+        lastKnownRate = initialRate;
+
+        this.modelDistance = modelDistance;
+        this.modelRate = modelRate;
+        this.modelAccel = modelAccel;
+    }
+
+    /**
+     * Gets the latest value from the encoder and updates internal state of the filter.
      */
     public void update() {
         final long now = System.currentTimeMillis();
-        final double dt = (now - lastKnownDistanceTime) / 1000.0;
         final double encoderDistance = encoder.getDistance();
+        final double encoderRate = encoder.getRate();
+
+        update(now, encoderDistance, encoderRate);
+
+        System.out.println(String.format("DATA,%d,%5.3f,%5.3f,%5.3f,%5.3f,%5.3f",
+            now, encoderDistance, encoderRate, modelDistance, modelRate, modelAccel));
+
+        SmartDashboard.putNumber("EncoderEstimater/distance", modelDistance);
+        SmartDashboard.putNumber("EncoderEstimater/rate", modelRate);
+        SmartDashboard.putNumber("EncoderEstimater/accel", modelAccel);
+    }
+
+    /**
+     * Update the internal state of the filter.
+     */
+    public void update(long nowMS, double encoderDistance, double encoderRate) {
+        final double dt = (nowMS - lastKnownDistanceTime) / 1000.0;
         
         if (encoderDistance != lastKnownDistance) {
             // New encoder distance available. Update the model using the measured value.
             modelDistance = encoderDistance;
             modelRate = (modelDistance - lastKnownDistance) / dt;
             modelAccel = (modelRate - lastKnownRate) / dt;
+            modelAccel = 0;
 
             lastKnownRate = modelRate;
             lastKnownDistance = modelDistance;
-            lastKnownDistanceTime = now;
+            lastKnownDistanceTime = nowMS;
         } else {
-            // Encoder value didn't change, estimate new model values.
+            // Encoder value didn't change, predict new model values.
             modelDistance = lastKnownDistance + modelRate * dt;
             modelRate = lastKnownRate + modelAccel * dt;
         }
-
-        System.out.println(String.format("DATA,%d,%5.3f,%5.3f,%5.3f,%5.3f,%5.3f",
-            now, encoder.getDistance(), encoder.getRate(), modelDistance, modelRate, modelAccel));
-
-        SmartDashboard.putNumber("EncoderEstimater/distance", modelDistance);
-        SmartDashboard.putNumber("EncoderEstimater/rate", modelRate);
-        SmartDashboard.putNumber("EncoderEstimater/accel", modelAccel);
     }
 
     public double getDistance() {
