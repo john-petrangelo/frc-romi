52813a597bcfafad84a9f55bfe422521078e7c1e
John Petrangelo <>
2021-06-06 21:58:32 -0400

    Added experimental Kalman filter to the drive train for testing

--- src/main/java/frc/robot/subsystems/RomiDrivetrain.java
+++ src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.filters.ApacheCommonsKalman;
 import frc.robot.filters.TrashCompactor;
 import frc.robot.speedcontrollers.FeedforwardSpeedController;
 
@@ -25,6 +26,9 @@ public class RomiDrivetrain extends SubsystemBase {
   // The left and right wheel sensors.
   private final TrashCompactor leftWheelSensor;
   private final TrashCompactor rightWheelSensor;
+
+  // Experimental Kalman filter
+  private final ApacheCommonsKalman kalman;
 
   // Limit the amount of change allowed per iteration for each sensor.
   private final FeedforwardSpeedController leftFFController;
@@ -82,14 +86,7 @@ public class RomiDrivetrain extends SubsystemBase {
       0.5561, 0.2666,  // RF
       0.7771, 0.2545); // RB
 
-  // Self-generated feed forward parameters for Romi wth block of wood on the roof.
-  private final static Characteristics heavyData = new Characteristics(
-    0.466, 0.256,  // LF
-    0.842, 0.259,  // LB
-    0.739, 0.276,  // RF
-    0.945, 0.260); // RB
-    
-  private Characteristics data = heavyData;
+  private Characteristics data = myData;
 
   /**
    * Creates a new RomiDrivetrain.
@@ -103,6 +100,9 @@ public class RomiDrivetrain extends SubsystemBase {
     leftWheelSensor.getEncoder().setDistancePerPulse(INCHES_PER_TICK);
     rightWheelSensor = new TrashCompactor(new Encoder(6, 7));
     rightWheelSensor.getEncoder().setDistancePerPulse(INCHES_PER_TICK);
+
+    // TODO Experimental Kalman filter
+    kalman = new ApacheCommonsKalman(leftWheelSensor.getEncoder(), rightWheelSensor.getEncoder());
 
     // Create the speed controllers used for the various test modes.
     leftFFController = new FeedforwardSpeedController("L", leftMotor,
@@ -146,6 +146,7 @@ public class RomiDrivetrain extends SubsystemBase {
   public void resetEncoders() {
     leftWheelSensor.reset();
     rightWheelSensor.reset();
+    kalman.reset();
   }
 
   public double getLeftDistanceInches() {
@@ -197,5 +198,28 @@ public class RomiDrivetrain extends SubsystemBase {
   public void periodic() {
     leftWheelSensor.update();
     rightWheelSensor.update();
+    kalman.predict();
+    kalman.update();
+
+    SmartDashboard.putNumber("tc/distance(left)", getLeftDistanceInches());
+    SmartDashboard.putNumber("tc/distance(right)", getRightDistanceInches());
+    SmartDashboard.putNumber("tc/rate(left)", getLeftRate());
+    SmartDashboard.putNumber("tc/rate(right)", getRightRate());
+
+    SmartDashboard.putNumber("kf/distance(left)", kalman.getLeftPosition());
+    SmartDashboard.putNumber("kf/distance(right)", kalman.getRightPosition());
+    SmartDashboard.putNumber("kf/rate(left)", kalman.getLeftRate());
+    SmartDashboard.putNumber("kf/rate(right)", kalman.getRightRate());
+
+//    kalman.logP();
+    logData();
+  }
+
+  private void logData() {
+    System.out.printf("CSV,%d,%f,%f\n",
+      System.currentTimeMillis(),
+      getLeftRate(),
+      getRightRate()
+    );
   }
 }
