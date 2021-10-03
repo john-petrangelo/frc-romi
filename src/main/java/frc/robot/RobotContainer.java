419e19c92d940823657671efc0fe78d6037dce29
John Petrangelo <>
2021-10-02 21:05:49 -0400

    Improved comments

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,22 +21,24 @@ import frc.robot.subsystems.RomiDrivetrain;
  * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
+  // Subsystems
   private final RomiGyro gyro = new RomiGyro();
   private final RomiDrivetrain drivetrain = new RomiDrivetrain(gyro);
 
+  // User interface objects
   private final Joystick controller = new Joystick(0);
-  // private final JoystickButton buttonA = new JoystickButton(controller, Buttons.A.value);
+  private final JoystickButton buttonA = new JoystickButton(controller, Buttons.A.value);
   private final JoystickButton buttonB = new JoystickButton(controller, Buttons.B.value);
   private final JoystickButton buttonX = new JoystickButton(controller, Buttons.X.value);
   private final JoystickButton buttonY = new JoystickButton(controller, Buttons.Y.value);
   private final JoystickButton buttonBack = new JoystickButton(controller, Buttons.Back.value);
-  // private final JoystickButton buttonStart = new JoystickButton(controller, Buttons.Start.value);
+  private final JoystickButton buttonStart = new JoystickButton(controller, Buttons.Start.value);
   private final JoystickButton povUp = new JoystickButton(controller, Buttons.POVup.value);
   private final JoystickButton povDown = new JoystickButton(controller, Buttons.POVdown.value);
   private final JoystickButton povLeft = new JoystickButton(controller, Buttons.POVleft.value);
   private final JoystickButton povRight = new JoystickButton(controller, Buttons.POVright.value);
-  // private final JoystickButton leftBumper = new JoystickButton(controller, Buttons.BumperLeft.value);
-  // private final JoystickButton rightBumper = new JoystickButton(controller, Buttons.BumperRight.value);
+  private final JoystickButton leftBumper = new JoystickButton(controller, Buttons.BumperLeft.value);
+  private final JoystickButton rightBumper = new JoystickButton(controller, Buttons.BumperRight.value);
 
   public RobotContainer() {
     // The default command is run when no other commands are active.
@@ -49,25 +51,14 @@ public class RobotContainer {
   }
  
   /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Connect the controller buttons to commands
    */
   private void configureButtonBindings() {
     povUp.whenPressed(new DriveDistanceCmd(36.0, drivetrain));
     povDown.whenPressed(new DriveDistanceCmd(-36.0, drivetrain));
-    // povUp.whenPressed(new DriveForwardWithGyro(36.0, drivetrain, gyro));
-    // povDown.whenPressed(new DriveForwardWithGyro(-36.0, drivetrain, gyro));
-    // povUp.whenPressed(new DriveTrapezoid(36.0, drivetrain));
-    // povDown.whenPressed(new DriveTrapezoid(-36.0, drivetrain));
 
     povLeft.whenPressed(new TurnDegreesCmd(-73, drivetrain, gyro));
     povRight.whenPressed(new TurnDegreesCmd(73, drivetrain, gyro));
-    // povLeft.whenPressed(new TurnToAngleWithPID(-90, drivetrain, gyro));
-    // povRight.whenPressed(new TurnToAngleWithPID(90, drivetrain, gyro));
-    // povLeft.whenPressed(new TurnTrapezoid(-90.0, drivetrain, gyro));
-    // povRight.whenPressed(new TurnTrapezoid(90.0, drivetrain, gyro));
 
     buttonBack.whenPressed(()  -> gyro.reset());
   }
